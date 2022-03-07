84675bf36ce146d3711a5a14068a4af09b78c7f8
Sardar Manmeet Singh <ss186092@ncr.com>
2022-03-07 03:47:55 -0500

    Adding new Project

--- InterfaceDemo/Program.cs
+++ InterfaceDemo/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using InterfaceDemo;
 namespace InterfaceDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Ishape Rectangleshape = new Rectangle();
+            Ishape SquareShape = new Square();
+            Rectangleshape.PrintShape();
+            SquareShape.PrintShape();
+            Console.ReadKey();
         }
     }
 }
