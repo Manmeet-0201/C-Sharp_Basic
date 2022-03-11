e129737b308dcf08ba40f3d5e11d33cb485fe25e
Sardar Manmeet Singh <ss186092@ncr.com>
2022-03-11 00:14:01 -0500

    Adding new changes

---
+++ InternalDemo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternalDemo;
+namespace InternalDemo1
+{
+    class Program:Class1
+    {
+        static void Main(string[] args)
+        {
+            Class1 Class1Object = new Class1();
+            //Class1.x;
+            //Above statement gives us error
+            Console.WriteLine(new Program().y);
+        }
+    }
+}
