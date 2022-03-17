6824146867c34d5d2ea9a60225f50d48423578b1
Sardar Manmeet Singh <ss186092@ncr.com>
2022-03-16 23:35:11 -0400

    Adding new project Calculator and Polymorphism

--- PolymorphismTestProject/Properties/AssemblyInfo.cs
+++ PolymorphismTestProject/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SalaryCalculatorTestProject")]
+[assembly: AssemblyTitle("PolymorphismTestProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SalaryCalculatorTestProject")]
+[assembly: AssemblyProduct("PolymorphismTestProject")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("955c7c69-8c7a-47d3-a4a4-20f36e304011")]
+[assembly: Guid("f16c4724-ce96-4aaa-9856-9ad61a370ae7")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
